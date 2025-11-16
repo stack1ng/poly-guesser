74ddf23bd8b779da594720fa8d558762d76c4864
nil <tomeaton76@gmail.com>
2025-11-16 11:54:20 -0800

    Update game-router.tsx

--- app/game/[id]/game-router.tsx
+++ app/game/[id]/game-router.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable react-hooks/immutability */
 "use client";
 
 import { ClientGameState } from "@/lib/game/state-types";
 import { useGameState } from "@/lib/useGameState";
-import { use } from "react";
+import { use, useMemo } from "react";
 import { Lobby } from "./(lobby)/lobby";
 import { InPlay } from "./(inplay)/in-play";
 import { Ended } from "./(ended)/ended";
@@ -24,19 +25,21 @@ export function GameRouter({
 	const game = useGameState(gameId, initialState.clientGameState, usePolling);
 	console.log("game-router game", game);
 
-	let content: React.ReactNode;
-	switch (game.phase) {
-		case "joinable":
-			content = <Lobby game={game} />;
-			break;
-		case "in_play":
-			content = <InPlay game={game} />;
-			break;
-		case "ended":
-			return <Ended game={game} />;
-		default:
-			throw new Error(`Unknown game phase: ${game.phase}`);
-	}
+	const content = useMemo(() => {
+		switch (game.phase) {
+			case "joinable":
+				document.title = `IN LOBBY - ${game.topic}`;
+				return <Lobby game={game} />;
+			case "in_play":
+				document.title = `IN PLAY - ${game.topic}`;
+				return <InPlay game={game} />;
+			case "ended":
+				document.title = `GAME OVER - ${game.topic}`;
+				return <Ended game={game} />;
+			default:
+				throw new Error(`Unknown game phase: ${game.phase}`);
+		}
+	}, [game]);
 
 	return <LeaderboardLayout game={game}>{content}</LeaderboardLayout>;
 }
