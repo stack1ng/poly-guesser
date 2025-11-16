4c7473622ebb324d571c06458ce411a3da13f029
nil <tomeaton76@gmail.com>
2025-11-16 00:14:42 -0800

    minor improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
+"use client";
+
 import { Logo } from "@/components/logo";
 import { Card } from "@/components/ui/card";
-import Link from "next/link";
+import { cn } from "@/lib/utils";
+import Link, { useLinkStatus } from "next/link";
 
 export default function Home() {
 	return (
 		<div className="flex flex-col text-5xl text-white gap-4">
 			<Logo />
 			<Link href="/game/new" prefetch={false}>
-				<Card className="grid place-items-center size-96 border-emerald-500 border bg-emerald-500/50 hover:bg-emerald-500 transition-colors hover:text-white">
-					New Game!
-				</Card>
+				<NewGameCard />
 			</Link>
 			<Link href="/game/join" prefetch={true}>
 				<Card className="grid place-items-center h-24 w-full border-sky-500 border bg-sky-500/50 hover:bg-sky-500 transition-colors hover:text-white">
@@ -17,5 +18,22 @@ export default function Home() {
 				</Card>
 			</Link>
 		</div>
+	);
+}
+
+function NewGameCard() {
+	const { pending } = useLinkStatus();
+
+	return (
+		<Card
+			className={cn(
+				"grid place-items-center size-96 border-emerald-500 border bg-emerald-500/50 hover:bg-emerald-500 transition-colors hover:text-white",
+				{
+					"animate-pulse": pending,
+				}
+			)}
+		>
+			New Game!
+		</Card>
 	);
 }
---
+++ drizzle/0002_faithful_ravenous.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "games" ADD COLUMN "topic" text;
